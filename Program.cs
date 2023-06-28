571fcd31c547205bd0fdbbba8f0bdf29cf59efa5
b6e6b6r6a <113089548+b6e6b6r6a@users.noreply.github.com>
2023-06-28 10:20:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace экз
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string oz = Console.ReadLine();
+            Console.WriteLine($"Поздравляю, {name}! У Вас автоматом {oz} за экзамен.");
+        }
+    }
+}
